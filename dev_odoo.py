f0864b850c562b8a9fd7b3cd694b9356c564adf8
Ninux <htetmyat@global-connect.asia>
2024-07-12 09:27:52 +0630

    [FIX] dev_odoo.py error message

--- dev_odoo.py
+++ dev_odoo.py
@@ -21,10 +21,7 @@ def find_conf_file(path, version):
 
 # Check the directory is present and can be accessed by current user.
 def is_root(path):
-    if type(path) == str:
-        return os.path.isdir(path) and os.access(path, os.R_OK)
-    for d in path:
-        return os.path.isdir(d) and os.access(d, os.R_OK)
+    return os.path.isdir(path) and os.access(path, os.R_OK)
 
 
 def main():
@@ -44,7 +41,7 @@ def main():
     name = args.project
     venv = odoo_bin = config_file = None
     if version and name:
-        if is_root(ODOO_ROOT_DIR) and is_root(PROJECT_ROOT) and is_root(VIRTUAL):
+        if all(map(is_root, [ODOO_ROOT_DIR[0], PROJECT_ROOT[0], VIRTUAL[0]])):
             venv = os.path.join(VIRTUAL[0],
                                 f'{ENV_PREFIX[0] if ENV_PREFIX is not None else ''}{version}{ENV_POSTFIX[0] if 
                                 ENV_POSTFIX is not None else ''}/bin/activate')
@@ -53,9 +50,11 @@ def main():
             project_dir = os.path.join(PROJECT_ROOT[0], name)
             config_file = f"{find_conf_file(project_dir, version)[0]}"
         else:
-            for root in [ODOO_ROOT_DIR, PROJECT_ROOT, VIRTUAL]:
+            p = ['ODOO_ROOT_DIR', 'PROJECT_ROOT', 'VIRTUAL']
+            for i, root in enumerate([ODOO_ROOT_DIR, PROJECT_ROOT, VIRTUAL]):
                 if not is_root(root):
-                    print(f'"{root[0]}" directory was not found in the your system path')
+                    print(f'"{root[0]}"  directory was not found in the your system path or cannot access the directory.')
+                    print(f'Please re-config your {p[i]} at settings.py!')
     else:
         print(f"Unsupported version: {version} or Project: {name} are not found")
         sys.exit(1)
