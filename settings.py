b5f957b3e18e8012558416c3811036b6ed187693
Ninux <htetmyat@global-connect.asia>
2024-09-21 20:51:56 +0630

    fix settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ HOME = os.environ.get('HOME'),
 
 PROJECT_ROOT = os.path.join(os.environ.get('HOME'), 'Projects/Odoo/'),  # set your project root directory
 
-VIRTUAL = os.path.join(os.environ.get('HOME'), 'Env'),  # change your python environment root directory
+VIRTUAL = os.path.join(os.environ.get('HOME'), 'pyenv'),  # change your python environment root directory
 
 # 'pre_version_post'
 
@@ -14,7 +14,7 @@ ENV_PREFIX = 'odoo',  # if ENV_PREFIX is not None: pre_version -> {pre}{version}
 
 ENV_POSTFIX = None  # if ENV_POSTFIX is not none: version_postfix -> {version}{post}
 
-ODOO_ROOT_DIR = '/var/lib/odoo/',  # change your odoo root directory
+ODOO_ROOT_DIR = os.path.join(HOME[0], '.odoo'),  # change your odoo root directory
 
 ODOO_VERSION_PREFIX = 'o'
 
