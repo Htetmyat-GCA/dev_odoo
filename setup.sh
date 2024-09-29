5c48a381e16b8d7670af89959feba31869e1ed97
Ninux <htetmyat@global-connect.asia>
2024-09-29 18:38:33 +0630

    Changes to be committed: 	deleted:    setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-pip install -r $(pwd)/requirements.txt
-activate-global-python-argcomplete --user
-chmod +x $(pwd)/run
-eval "$(register-python-argcomplete $(pwd)/run)"
-echo eval "$(register-python-argcomplete $(pwd)/run)" >> ~/.bashrc
-source ~/.bashrc
-exit
